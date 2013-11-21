304603a850663f7ea3c99c9ccff27aedaabb666c
Kouhei Sutou <kou@clear-code.com>
2013-11-21 15:53:26 +0900

    Move requires to file that are used

--- lib/droonga/client.rb
+++ lib/droonga/client.rb
@@ -15,10 +15,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require "socket"
-require "msgpack"
-require "fluent-logger"
-
 require "droonga/client/version"
 require "droonga/client/error"
 require "droonga/client/connection/droonga_protocol"
--- lib/droonga/client/connection/droonga_protocol.rb
+++ lib/droonga/client/connection/droonga_protocol.rb
@@ -15,6 +15,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+require "socket"
+require "msgpack"
+require "fluent-logger"
+
 module Droonga
   class Client
     module Connection
